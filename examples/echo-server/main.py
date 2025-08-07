2cb9f01d10337eccd54b7954598189331c85ee79
Hanyeol Cho <hanyeol.cho@gmail.com>
2025-08-07 10:53:11 +0900

    Add an example: echo-server

---
+++ examples/echo-server/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+@app.post("/echo")
+async def echo(request: Request):
+    body = await request.json()
+    return JSONResponse(content={"echo": body})

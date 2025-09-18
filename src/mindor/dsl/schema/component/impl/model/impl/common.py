df007baa47061e7161a4cf7ce272cbd3f207b466
Hanyeol Cho <hanyeol.cho@gmail.com>
2025-09-18 22:54:45 +0900

    Clean up the model configuration

--- src/mindor/dsl/schema/component/impl/model/impl/common.py
+++ src/mindor/dsl/schema/component/impl/model/impl/common.py
@@ -3,32 +3,15 @@ from enum import Enum
 from pydantic import BaseModel, Field
 from pydantic import model_validator
 from ...common import CommonComponentConfig, ComponentType
-from .types import ModelTaskType
 
-class DeviceMode(str, Enum):
-    SINGLE = "single"
-    AUTO   = "auto"
-
-class ModelPrecision(str, Enum):
-    AUTO     = "auto"
-    FLOAT32  = "float32"
-    FLOAT16  = "float16"
-    BFLOAT16 = "bfloat16"
-
-class AttentionMode(str, Enum):
-    SDPA              = "sdpa"
-    EAGER             = "eager"
-    FLASH_ATTENTION_2 = "flash_attention_2"
-    MEM_EFFICIENT     = "mem_efficient"
-
-class Quantization(str, Enum):
-    NONE = "none"
-    FP16 = "fp16"
-    BF16 = "bf16"
-    INT8 = "int8"
-    INT4 = "int4"
-    FP4  = "fp4"
-    NF4  = "nf4"
+class ModelTaskType(str, Enum):
+    TEXT_GENERATION     = "text-generation"
+    CHAT_COMPLETION     = "chat-completion"
+    TEXT_CLASSIFICATION = "text-classification" 
+    TEXT_EMBEDDING      = "text-embedding"
+    IMAGE_TO_TEXT       = "image-to-text"
+    IMAGE_GENERATION    = "image-generation"
+    IMAGE_UPSCALE       = "image-upscale"
 
 class ModelDriver(str, Enum):
     HUGGINGFACE = "huggingface"
@@ -47,6 +30,25 @@ class ModelFormat(str, Enum):
     ONNX        = "onnx"
     GGUF        = "gguf"
     TENSORRT    = "tensorrt"
+
+class ModelPrecision(str, Enum):
+    AUTO     = "auto"
+    FLOAT32  = "float32"
+    FLOAT16  = "float16"
+    BFLOAT16 = "bfloat16"
+
+class ModelQuantization(str, Enum):
+    NONE = "none"
+    FP16 = "fp16"
+    BF16 = "bf16"
+    INT8 = "int8"
+    INT4 = "int4"
+    FP4  = "fp4"
+    NF4  = "nf4"
+
+class DeviceMode(str, Enum):
+    SINGLE = "single"
+    AUTO   = "auto"
 
 class CommonModelConfig(BaseModel):
     provider: ModelProvider = Field(..., description="Model provider.")
@@ -81,7 +83,7 @@ class CommonModelComponentConfig(CommonComponentConfig):
     device_mode: DeviceMode = Field(default=DeviceMode.AUTO, description="Device allocation mode.")
     device: str = Field(default="cpu", description="Computation device to use.")
     precision: Optional[ModelPrecision] = Field(default=None, description="Numerical precision to use when loading the model weights.")
-    quantization: Quantization = Field(default=Quantization.NONE, description="Quantization method.")
+    quantization: ModelQuantization = Field(default=ModelQuantization.NONE, description="Quantization method.")
     low_cpu_mem_usage: bool = Field(default=False, description="Load model with minimal CPU RAM usage.")
     fast_tokenizer: bool = Field(default=True, description="Whether to use the fast tokenizer if available.")
     max_seq_length: int = Field(default=2048, description="")

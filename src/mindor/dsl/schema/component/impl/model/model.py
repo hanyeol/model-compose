0b24037474110b1fa98fd32ee4956528953b2a3f
Hanyeol Cho <hanyeol.cho@gmail.com>
2025-09-24 18:34:25 +0900

    Add face embedding component

--- src/mindor/dsl/schema/component/impl/model/model.py
+++ src/mindor/dsl/schema/component/impl/model/model.py
@@ -11,6 +11,7 @@ ModelComponentConfig = Annotated[
         ImageToTextModelComponentConfig,
         ImageGenerationModelComponentConfig,
         ImageUpscaleModelComponentConfig,
+        FaceEmbeddingeModelComponentConfig
     ],
     Field(discriminator="task")
 ]

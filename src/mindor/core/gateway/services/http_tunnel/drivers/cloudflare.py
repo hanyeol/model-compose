ac2c71e71d4315b2a68fb37fa8f363bc10cfb2be
Hanyeol Cho <hanyeol.cho@gmail.com>
2025-09-26 22:03:07 +0900

    Improve code structure

---
+++ src/mindor/core/gateway/services/http_tunnel/drivers/cloudflare.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from typing import Type, Union, Literal, Optional, Dict, List, Tuple, Set, Annotated, Callable, Iterator, Any
+from abc import ABC, abstractmethod
+from mindor.dsl.schema.gateway import HttpTunnelGatewayConfig
+from mindor.core.logger import logging
+from ..base import CommonHttpTunnelGateway
+import asyncio
+
+if TYPE_CHECKING:
+    pass
+
+class CloudflareHttpTunnelGateway(CommonHttpTunnelGateway):
+    def __init__(self, config: HttpTunnelGatewayConfig):
+        super().__init__(config)

464f4117d8105b3dfcc625303dd004e223b7f200
Hanyeol Cho <hanyeol.cho@gmail.com>
2025-09-29 10:18:48 +0900

    Add websocket client utility

--- src/mindor/core/utils/time.py
+++ src/mindor/core/utils/time.py
@@ -16,15 +16,18 @@ def parse_duration(value: Union[str, float, int]) -> timedelta:
     if isinstance(value, (float, int)):
         return timedelta(seconds=value)
 
+    if value.endswith("ms"):
+        return timedelta(milliseconds=float(value[:-2]))
+
     if value.endswith("s"):
         return timedelta(seconds=float(value[:-1]))
-    
+
     if value.endswith("m"):
         return timedelta(minutes=float(value[:-1]))
-    
+
     if value.endswith("h"):
         return timedelta(hours=float(value[:-1]))
-    
+
     if value.endswith("d"):
         return timedelta(days=float(value[:-1]))
 

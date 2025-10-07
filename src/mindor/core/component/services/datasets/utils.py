0d7c662814ae444d79a6a1b0028549a4942ed63e
Hanyeol Cho <hanyeol.cho@gmail.com>
2025-10-07 09:04:24 +0900

    Clean up the codes

--- src/mindor/core/component/services/datasets/utils.py
+++ src/mindor/core/component/services/datasets/utils.py
@@ -1,6 +1,5 @@
+from typing import Dict, List, Any
 import re
-from typing import Dict, Any
-
 
 def format_template_example(template: str, example: Dict[str, Any]) -> str:
     column_names = set(re.findall(r"\{(\w+)\}", template))

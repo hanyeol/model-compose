0693e7b31d95aaeaa68c5c2ce68f02df5938a131
Hanyeol Cho <hanyeol.cho@gmail.com>
2026-06-01 12:32:23 +0900

    Improve code quality

--- src/mindor/core/errors.py
+++ src/mindor/core/errors.py
@@ -1,15 +1,5 @@
-from typing import Optional
-
 class TaskError(ValueError):
-    """ValueError variant carrying a stable error code for API responses."""
-
     code: str = "INVALID_REQUEST"
 
-    def __init__(self, message: str, code: Optional[str] = None):
-        super().__init__(message)
-        if code is not None:
-            self.code = code
-
 class ShutdownError(RuntimeError):
-    """Raised when an operation is attempted while the service is shutting down."""
     pass

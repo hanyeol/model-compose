c6bc5b60c598edafca908723187d14d650153cab
Hanyeol Cho <hanyeol.cho@gmail.com>
2026-06-21 11:15:21 +0900

    Offload PIL PNG encoding to a worker thread to keep the event loop responsive while streaming image frames

--- src/mindor/core/utils/streaming/image.py
+++ src/mindor/core/utils/streaming/image.py
@@ -2,7 +2,7 @@ from typing import Optional
 from collections.abc import AsyncIterator
 from .stream import StreamResource
 from PIL import Image as PILImage
-import io
+import asyncio, io
 
 _CONTENT_TYPE_MAP = {
     "png": "image/png",
@@ -41,15 +41,19 @@ class ImageStreamResource(StreamResource):
 
     async def _iterate_stream(self) -> AsyncIterator[bytes]:
         if not self._buffer:
-            self._buffer = io.BytesIO()
-            self.image.save(self._buffer, self._resolve_pil_format(self.format))
-            self._buffer.seek(0)
+            self._buffer = await asyncio.to_thread(self._write_to_buffer, self.image, self.format)
 
         while True:
             chunk = self._buffer.read(8192)  # Read in 8KB chunks
             if not chunk:
                 break
             yield chunk
+
+    def _write_to_buffer(self, image: PILImage.Image, format: str) -> io.BytesIO:
+        buffer = io.BytesIO()
+        image.save(buffer, self._resolve_pil_format(format))
+        buffer.seek(0)
+        return buffer
 
     def _resolve_content_type(self, format: str) -> str:
         return _CONTENT_TYPE_MAP.get(format, "application/octet-stream")
